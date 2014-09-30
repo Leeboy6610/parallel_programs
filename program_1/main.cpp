8fd2193cbecafbf1f649d6d4ca5e9cb9cf5cef71
Leo Schultz <leo@cuore.io>
2014-09-30 14:04:25 -0700

    Project Updated

---
+++ program_1/main.cpp
@@ -0,0 +1,21 @@
+// C++ Implementation
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <iostream>
+using namespace std;
+
+void finder ()
+{
+   ofstream datafile;
+   datafile.open ("Human_MtDNA.txt", ios::out | ios::app | ios::binary);
+   if (myfile.is_open()) {
+      cout << "Text File is Open" << endl;
+   }
+}
+
+int main ()
+{
+   finder ();
+}
--- program_1/main.py
+++ program_1/main.py
@@ -1,8 +1,20 @@
 # Python Implementation
+import time
 
-def dat_fun():
-    with open("Human_MtDNA.txt", "r") as ifile:
-        for line in ifile:
-            if not line:
-                break
-            yield line
+def main():
+	start_time = time.time()
+	lineno = 0
+	count = 0
+	with open('Human_MtDNA.txt', 'r') as inF:
+		for line in inF:
+			lineno += 1
+			if 'GAAAAT' in line:
+					count += 1
+					print 'Found in line ', lineno
+			# else:
+					# print 'Not Found in line ', lineno
+		runtime = time.time() - start_time
+		print count, 'Inputs found in', runtime, 'seconds!!!'
+		inF.close()
+
+main()
