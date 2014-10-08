6ba2a0c4e2925d8e8b34892e68fda3992c805760
Leo Schultz <leo@cuore.io>
2014-10-07 19:27:14 -0700

    ValGrind Leaks & Time Added

--- program_2/main.c
+++ program_2/main.c
@@ -1,10 +1,15 @@
+// C Implementation
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <pthread.h>
-# define BufferSize 10
+# include <sys/time.h>
 
-void *Producer();
-void *Consumer();
+# define BufferSize 10
+# define MAX 2000000
+
+void *producer();
+void *consumer();
 
 int BufferIndex=0;
 char *BUFFER;
@@ -15,23 +20,28 @@ pthread_mutex_t mVar=PTHREAD_MUTEX_INITIALIZER;
 
 int main()
 {    
+    struct timeval tim;
+    gettimeofday(&tim, NULL);
+    double start_time=tim.tv_sec+(tim.tv_usec/1000000.0);
+
     pthread_t ptid,ctid;
-    
     BUFFER=(char *) malloc(sizeof(char) * BufferSize);            
-    
-    pthread_create(&ptid,NULL,Producer,NULL);
-    pthread_create(&ctid,NULL,Consumer,NULL);
-    
+    pthread_create(&ptid,NULL,producer,NULL);
+    pthread_create(&ctid,NULL,consumer,NULL);
     pthread_join(ptid,NULL);
     pthread_join(ctid,NULL);
         
-    
+    gettimeofday(&tim, NULL);  
+    double end_time=tim.tv_sec+(tim.tv_usec/1000000.0);
+    double runtime = end_time - start_time;
+    printf("Program ran in %f seconds! \n", runtime);
     return 0;
 }
 
-void *Producer()
+void *producer()
 {    
-    for(;;)
+    int i;
+    for(i = 1; i <= MAX; i++)
     {
         pthread_mutex_lock(&mVar);
         if(BufferIndex==BufferSize)
@@ -43,12 +53,13 @@ void *Producer()
         pthread_mutex_unlock(&mVar);
         pthread_cond_signal(&Buffer_Not_Empty);        
     }    
-    
+    return 0;
 }
 
-void *Consumer()
+void *consumer()
 {
-    for(;;)
+    int i;
+    for(i = 1; i <= MAX; i++)
     {
         pthread_mutex_lock(&mVar);
         if(BufferIndex==-1)
@@ -58,5 +69,6 @@ void *Consumer()
         printf("Consume : %d \n",BufferIndex--);        
         pthread_mutex_unlock(&mVar);        
         pthread_cond_signal(&Buffer_Not_Full);                
-    }    
+    }
+    return 0;   
 }
